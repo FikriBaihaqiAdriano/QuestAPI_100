a214228e661e08b0a6d31f4c5157f87b7f148ac5
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-12-30 05:51:48 +0700

    menambahkan editviewmodel ke dalam PenyediaViewModel.kt

--- app/src/main/java/com/example/serverdatabase/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/serverdatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -3,6 +3,7 @@ package com.example.serverdatabase.ui.viewmodel
 import android.os.Build
 import androidx.annotation.RequiresExtension
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -23,6 +24,12 @@ object PenyediaViewModel {
         }
         initializer {
             DetailViewModel(
+                MahasiswaApplications().MahasiswaContainer.mahasiswaRepository
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
                 MahasiswaApplications().MahasiswaContainer.mahasiswaRepository
             )
         }
