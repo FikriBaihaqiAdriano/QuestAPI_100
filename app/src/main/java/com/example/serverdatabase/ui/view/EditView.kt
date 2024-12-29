17e765a4c5c9364ff31e65664bb17d36a3a86189
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-12-30 05:46:34 +0700

    membuat EditView

---
+++ app/src/main/java/com/example/serverdatabase/ui/view/EditView.kt
@@ -0,0 +1,68 @@
+package com.example.serverdatabase.ui.view
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.serverdatabase.ui.costumwidget.CostumeTopAppBar
+import com.example.serverdatabase.ui.navigation.DestinasiNavigasi
+import com.example.serverdatabase.ui.viewmodel.EditViewModel
+import com.example.serverdatabase.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "update"
+    override val titleRes = "Update Mhs"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditScreen(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate:()-> Unit,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEdit.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = onBack,
+            )
+        }
+    ){padding ->
+        EntryBody(
+            modifier = Modifier.padding(padding),
+            insertUiState = viewModel.updateUiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateMhs()
+                    delay(600)
+                    withContext(Dispatchers.Main){
+                        onNavigate()
+                    }
+                }
+            }
+        )
+    }
+}
