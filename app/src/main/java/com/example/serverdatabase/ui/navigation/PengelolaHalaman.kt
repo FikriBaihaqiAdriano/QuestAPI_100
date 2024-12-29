3127aed60cdc6725fa3e365026252d2fa0cecf74
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-12-30 05:32:25 +0700

    menambahkan destinasidetail ke PengelolaHalaman

--- app/src/main/java/com/example/serverdatabase/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/serverdatabase/ui/navigation/PengelolaHalaman.kt
@@ -5,12 +5,17 @@ import androidx.annotation.RequiresExtension
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.serverdatabase.ui.view.DestinasiDetail
+import com.example.serverdatabase.ui.view.DestinasiEdit
 import com.example.serverdatabase.ui.view.DestinasiEntry
 import com.example.serverdatabase.ui.view.DestinasiHome
+import com.example.serverdatabase.ui.view.DetailScreen
+import com.example.serverdatabase.ui.view.EditScreen
 import com.example.serverdatabase.ui.view.EntryMhsScreen
 import com.example.serverdatabase.ui.view.HomeScreen
 
@@ -39,6 +44,26 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                     }
                 }
             })
+        }
+    composable(DestinasiDetail.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiDetail.NIM){
+                    type = NavType.StringType
+                }
+            )
+        ){
+            val nim = it.arguments?.getString(DestinasiDetail.NIM)
+            nim?.let { nim ->
+                DetailScreen(
+                    onBackClick = {
+                        navController.popBackStack()
+                    },
+                    onEditClick = {
+                        navController.navigate("${DestinasiEdit.route}/$it")
+                    },
+                    nim = nim
+                )
+            }
         }
     }
 }
